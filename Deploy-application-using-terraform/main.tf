cef5a6377d6069361c7c086385ec846516a2d577
Akhil Kommineni <akhilkommineni6598@gmail.com>
2025-12-22 17:57:10 +0530

    added

---
+++ Deploy-application-using-terraform/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_security_group" "python_sg" {
+  name        = "python-app-sg"
+  description = "Allow SSH + HTTP"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "python_server" {
+  ami                    = var.ami_id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.python_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "Terraform-python-app"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/Users/Asus/downloads/pro.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "app.py"
+    destination = "/home/ec2-user/app.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install python3-pip -y",
+      "pip install flask",
+      "nohup python3 /home/ec2-user/app.py &"
+    ]
+  }
+}
---
+++ Deploy-application-using-terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "instance_public_ip" {
+  value = aws_instance.python_server.public_ip
+}
