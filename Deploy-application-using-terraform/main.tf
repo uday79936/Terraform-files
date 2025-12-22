69c35b3346f2e6456d066f1f884b164bec63728f
Uday Sairam Kommineni <67004722+uday79936@users.noreply.github.com>
2025-12-22 17:58:50 +0530

    Update main.tf

--- Deploy-application-using-terraform/main.tf
+++ Deploy-application-using-terraform/main.tf
@@ -19,8 +19,8 @@ resource "aws_security_group" "python_sg" {
 
   ingress {
     description = "HTTP"
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
